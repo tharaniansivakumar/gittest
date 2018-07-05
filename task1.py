2b1782f6e1b286e6fbe08e44f09a9ca2256222f6
tharaniansivakumar <tharanian1996@gmail.com>
2018-07-05 12:17:39 +0530

    new file added

---
+++ task1.py
@@ -0,0 +1,14 @@
+n=int(input("Enter the total number"))
+li=[]
+for  i in range(n):
+	t=[]
+	tu=()
+	a=int(input("enter the number"))
+	t.append(a)
+	# sdsd
+	t.append(a*a)
+	tu=tuple(t)
+	li.append(tu)
+print(li)
+
+# dfdf
